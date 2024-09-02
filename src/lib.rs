d2c0a15742fdcc3e6434ebc13f0cd1a8fbe6ca38
styler <63294765+matstyler@users.noreply.github.com>
2024-09-02 11:25:46 +0200

    fix: ConvertToAssets decoding (#1)

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,5 @@
 mod pb;
 mod abi;
-mod constants;
 mod calls;
 
 use substreams_entity_change::pb::entity::EntityChanges;
@@ -20,7 +19,7 @@ fn map_erc4626_tokens(block: eth::v2::Block) -> Result<Erc4626Tokens, substreams
 
             for (log, _) in transaction.logs_with_calls() {
                 match Deposit::match_and_decode(&log) {
-                    Some(deposit) => {
+                    Some(_deposit) => {
                         let erc4626_token: Option<Erc4626Token> = get_erc4626token(log.address.clone());
 
                         if erc4626_token.is_some() {
@@ -47,6 +46,7 @@ pub fn graph_out(tokens: Erc4626Tokens) -> Result<EntityChanges, substreams::err
         tables
             .create_row("ERC4626", contract.address)
             .set("decimals", contract.decimals)
+            .set("symbol", contract.symbol)
             .set("convertToAssetsRate", contract.convert_to_assets_rate)
             .set("convertToSharesRate", contract.convert_to_shares_rate);
     }
