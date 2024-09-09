3a8e657313990725d806a8753901fa6bc4807f9e
styler <63294765+matstyler@users.noreply.github.com>
2024-09-09 10:02:59 +0200

    feat: Rate entity (#6)

--- src/lib.rs
+++ src/lib.rs
@@ -8,7 +8,7 @@ use substreams_ethereum::pb::eth;
 use substreams_ethereum::{Event};
 use crate::abi::erc4626::events::Deposit;
 use crate::calls::get_erc4626token;
-use crate::pb::vision::{Erc4626Token, Erc4626Tokens};
+use crate::pb::vision::{Erc4626Token, Erc4626Tokens, Rate, Rates};
 
 #[substreams::handlers::map]
 fn map_erc4626_tokens(block: eth::v2::Block) -> Result<Erc4626Tokens, substreams::errors::Error> {
@@ -38,7 +38,26 @@ fn map_erc4626_tokens(block: eth::v2::Block) -> Result<Erc4626Tokens, substreams
 }
 
 #[substreams::handlers::map]
-pub fn graph_out(tokens: Erc4626Tokens) -> Result<EntityChanges, substreams::errors::Error> {
+fn map_rates(erc4626tokens: Erc4626Tokens, block: eth::v2::Block) -> Result<Rates, substreams::errors::Error> {
+    let mut rates: Vec<Rate> = Vec::new();
+
+    erc4626tokens.erc4626_tokens.into_iter()
+        .for_each(|erc4626| {
+            rates.push(Rate {
+                id: format!("{}-{}", erc4626.address, block.number),
+                timestamp: block.timestamp_seconds(),
+                block_number: block.number,
+                erc4626: erc4626.address,
+                convert_to_assets_rate: erc4626.convert_to_assets_rate,
+                convert_to_shares_rate: erc4626.convert_to_shares_rate,
+            });
+        });
+
+    Ok(Rates { rates })
+}
+
+#[substreams::handlers::map]
+pub fn graph_out(tokens: Erc4626Tokens, rates: Rates) -> Result<EntityChanges, substreams::errors::Error> {
     // hash map of name to a table
     let mut tables = Tables::new();
 
@@ -49,9 +68,17 @@ pub fn graph_out(tokens: Erc4626Tokens) -> Result<EntityChanges, substreams::err
             .set("symbol", contract.symbol)
             .set("asset", contract.asset)
             .set("totalAssets", contract.total_assets)
-            .set("totalSupply", contract.total_supply)
-            .set("convertToAssetsRate", contract.convert_to_assets_rate)
-            .set("convertToSharesRate", contract.convert_to_shares_rate);
+            .set("totalSupply", contract.total_supply);
+    }
+
+    for rate in rates.rates.into_iter() {
+        tables
+            .create_row("Rate", rate.id)
+            .set("timestamp", rate.timestamp)
+            .set("blockNumber", rate.block_number)
+            .set("erc4626", rate.erc4626)
+            .set("convertToAssetsRate", rate.convert_to_assets_rate)
+            .set("convertToSharesRate", rate.convert_to_shares_rate);
     }
 
     Ok(tables.to_entity_changes())
